385d0fd7afc5f13def8620fe55ae91fdda8d7ccb
小木木 <d360493454@163.com>
2023-03-31 21:05:42 +0800

    创建文件

---
+++ scripts/immortalwrt/diy-master.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#更改默认地址为192.168.8.1
+sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
+
+#更改r8152默认驱动
+sed -i 's/kmod-usb-net-rtl8152/kmod-usb-net-rtl8152-vendor/g' target/linux/rockchip/image/armv8.mk
+
+#添加内核配置
+sed -i '$a CONFIG_PHY_ROCKCHIP_INNO_USB3=y' target/linux/rockchip/armv8/config-5.15
+
+
+#克隆的源码放在small文件夹
+mkdir package/small
+pushd package/small
+
+#克隆源码
+
+git clone -b main https://github.com/xiaorouji/openwrt-passwall2.git
+git clone -b v5 https://github.com/sbwml/luci-app-mosdns.git
+
+popd
