c7342eb6a35841febe9cd362556388f39638b0c0
小木木 <d360493454@163.com>
2023-03-31 22:23:45 +0800

    替换源码原本的uboot.dts

--- scripts/immortalwrt/diy-master.sh
+++ scripts/immortalwrt/diy-master.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#删除自带u-boot.dts
+rm -rf package/boot/uboot-rockchip/src/arch/arm/dts/rk3328-orangepi-r1-plus-lts-u-boot.dtsi
+
 #更改默认地址为192.168.8.1
 sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
 
