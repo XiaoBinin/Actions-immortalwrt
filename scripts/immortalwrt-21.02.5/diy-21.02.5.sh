64040bca155ef458421145d5af02f6a32359bcbd
小木木 <d360493454@163.com>
2023-04-06 07:43:15 +0800

    上传21.02的配置文件，删除可能冲突的插件

--- scripts/immortalwrt-21.02.5/diy-21.02.5.sh
+++ scripts/immortalwrt-21.02.5/diy-21.02.5.sh
@@ -20,10 +20,10 @@ rm -rf ./feeds/luci/applications/luci-app-vssr
 #rm -rf package/boot/uboot-rockchip/src/arch/arm/dts/rk3328-orangepi-r1-plus-lts-u-boot.dtsi
 
 #更改默认地址为192.168.8.1
-sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
 
 #更改r8152默认驱动
-sed -i 's/kmod-usb-net-rtl8152/kmod-usb-net-rtl8152-vendor/g' target/linux/rockchip/image/armv8.mk
+#sed -i 's/kmod-usb-net-rtl8152/kmod-usb-net-rtl8152-vendor/g' target/linux/rockchip/image/armv8.mk
 
 #添加内核配置
 #sed -i '$a CONFIG_PHY_ROCKCHIP_INNO_USB3=y' target/linux/rockchip/armv8/config-5.15
@@ -37,9 +37,14 @@ sed -i 's/kmod-usb-net-rtl8152/kmod-usb-net-rtl8152-vendor/g' target/linux/rockc
 #openwrt-packages
 git clone https://github.com/kenzok8/openwrt-packages.git package/opackages
 rm -rf package/opackages/luci-app-mosdns
+rm -rf package/opackages/luci-app-argon-config
+rm -rf package/opackages/luci-theme-argonne 
+
 #wall
 git clone https://github.com/kenzok8/wall.git package/wall
 rm -rf package/wall/mosdns
+rm -rf package/wall/upx
+
 #mosdns
 git clone -b v5 https://github.com/sbwml/luci-app-mosdns.git package/mosdns
 
