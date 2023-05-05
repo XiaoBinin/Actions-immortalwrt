4c50dea27b994de42fbf5e8853012de2cd28556e
小木木 <d360493454@163.com>
2023-05-05 09:56:49 +0800

    修改master源码补丁和diy.sh

--- scripts/master/diy.sh
+++ scripts/master/diy.sh
@@ -7,19 +7,22 @@ rm -rf package/boot/uboot-rockchip/src/arch/arm/dts/rk3328-orangepi-r1-plus-lts-
 sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
 
 #更改r8152默认驱动
-sed -i 's/kmod-usb-net-rtl8152/kmod-usb-net-rtl8152-vendor/g' target/linux/rockchip/image/armv8.mk
+#sed -i 's/kmod-usb-net-rtl8152/kmod-usb-net-rtl8152-vendor/g' target/linux/rockchip/image/armv8.mk
 
 #添加内核配置
 sed -i '$a CONFIG_PHY_ROCKCHIP_INNO_USB3=y' target/linux/rockchip/armv8/config-5.15
 
 
 #克隆的源码放在small文件夹
-mkdir package/small
-pushd package/small
+#mkdir package/small
+#pushd package/small
 
 #克隆源码
+#adguardhome
+git clone -b main --depth 1 https://github.com/XiaoBinin/luci-app-adguardhome.git package/adg
+#passwall2
+git clone -b main --depth 1 https://github.com/xiaorouji/openwrt-passwall2.git package/pw2
+#mosdns
+git clone -b v5 --depth 1 https://github.com/sbwml/luci-app-mosdns.git package/mosdns
 
-git clone -b main --depth 1 https://github.com/xiaorouji/openwrt-passwall2.git
-git clone -b v5 --depth 1 https://github.com/sbwml/luci-app-mosdns.git
-
-popd
+#popd
