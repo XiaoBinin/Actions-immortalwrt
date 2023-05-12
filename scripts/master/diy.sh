71a66ea94811675bc2b70f556324659c7fb19936
小木木 <d360493454@163.com>
2023-05-12 21:31:41 +0800

    Revert "删除usb3内核配置"
    This reverts commit 8a3c70dd1afe33ac6effe36459b759ed99419546.

--- scripts/master/diy.sh
+++ scripts/master/diy.sh
@@ -10,7 +10,7 @@ sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generat
 sed -i 's/kmod-usb-net-rtl8152/kmod-usb-net-rtl8152-vendor/g' target/linux/rockchip/image/armv8.mk
 
 #添加内核配置
-#sed -i '$a CONFIG_PHY_ROCKCHIP_INNO_USB3=y' target/linux/rockchip/armv8/config-5.15
+sed -i '$a CONFIG_PHY_ROCKCHIP_INNO_USB3=y' target/linux/rockchip/armv8/config-5.15
 
 
 #克隆的源码放在small文件夹
