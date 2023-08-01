4768ce2cd3382ceaa34dcac69d26f1d4fc76df25
小木木 <d360493454@163.com>
2023-08-01 20:41:02 +0800

    删除rk3328平台5.4内核以外的修改

--- scripts/master/diy.sh
+++ scripts/master/diy.sh
@@ -10,7 +10,7 @@ rm -rf package/boot/uboot-rockchip/src/arch/arm/dts/rk3328-orangepi-r1-plus-lts-
 sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
 
 #更改r8152默认驱动
-sed -i 's/kmod-usb-net-rtl8152/kmod-usb-net-rtl8152-vendor/g' target/linux/rockchip/image/armv8.mk
+#sed -i 's/kmod-usb-net-rtl8152/kmod-usb-net-rtl8152-vendor/g' target/linux/rockchip/image/armv8.mk
 
 #添加内核配置
 sed -i '$a CONFIG_PHY_ROCKCHIP_INNO_USB3=y' target/linux/rockchip/armv8/config-6.1
