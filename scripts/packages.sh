eec5078feebf184ba0c6399973e0be64b3c91b01
XiaoBinin <d360493454@163.com>
2024-12-28 06:49:24 +0800

    scripts : 删除feeds自带geoview

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -3,6 +3,7 @@
 #删除feeds中的插件
 # rm -rf ./feeds/packages/net/mosdns
 # rm -rf ./feeds/packages/net/v2ray-geodata
+rm -rf ./feeds/packages/net/geoview
 rm -rf ./feeds/packages/net/shadowsocks-libev
 rm -rf ./feeds/packages/net/chinadns-ng
 
