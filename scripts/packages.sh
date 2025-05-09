7a3353dac1cbf49aa50a76db6bd74b3253b8336f
XiaoBinin <128011031+XiaoBinin@users.noreply.github.com>
2025-05-10 07:03:55 +0800

    workflows : x86-64平台使用新格式，增加更多插件

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
 #删除feeds中的插件
-# rm -rf ./feeds/packages/net/mosdns
 # rm -rf ./feeds/packages/net/v2ray-geodata
-rm -rf ./feeds/packages/net/geoview
-rm -rf ./feeds/packages/net/shadowsocks-libev
-rm -rf ./feeds/packages/net/chinadns-ng
-
+rm -rf ./feeds/packages/net/{geoview,shadowsocks-libev,chinadns-ng,mosdns}
+rm -rf ./feeds/luci/applications/luci-app-mosdns
 
 #克隆依赖插件
 git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/pwpage
@@ -18,9 +15,9 @@ pushd package/small
 
 
 #adguardhome
-git clone -b 2023.10 --depth 1 https://github.com/XiaoBinin/luci-app-adguardhome.git
+git clone -b 2024.09.05 --depth 1 https://github.com/XiaoBinin/luci-app-adguardhome.git
 #lucky
-git clone -b main --depth 1 https://github.com/sirpdboy/luci-app-lucky.git
+git clone -b main --depth 1 https://github.com/gdy666/luci-app-lucky.git
 # #smartdns
 # git clone -b lede --depth 1 https://github.com/pymumu/luci-app-smartdns.git
 # git clone -b master --depth 1 https://github.com/pymumu/smartdns.git
@@ -31,7 +28,7 @@ git clone -b main --depth 1 https://github.com/sirpdboy/luci-app-lucky.git
 #passwall2
 git clone -b main --depth 1 https://github.com/xiaorouji/openwrt-passwall2.git
 # #mosdns
-# git clone -b v5 --depth 1 https://github.com/sbwml/luci-app-mosdns.git
+git clone -b v5 --depth 1 https://github.com/sbwml/luci-app-mosdns.git
 # #openclash
 # git clone -b master --depth 1 https://github.com/vernesong/OpenClash.git
 
--- scripts/preset-clash-core-amd64.sh
+++ scripts/preset-clash-core-amd64.sh
@@ -16,11 +16,11 @@ mkdir -p files/etc/openclash/core
 
 
 # openclash 的 dev内核
-CLASH_DEV_URL="https://github.com/vernesong/OpenClash/raw/core/master/dev/clash-linux-amd64.tar.gz"
+# CLASH_DEV_URL="https://github.com/vernesong/OpenClash/raw/core/master/dev/clash-linux-amd64.tar.gz"
 
 # openclash 的 TUN内核
-CLASH_TUN_VERSION=$(curl -sL https://github.com/vernesong/OpenClash/raw/core/master/core_version | head -n 2 | tail -n 1)
-CLASH_TUN_URL="https://github.com/vernesong/OpenClash/raw/core/master/premium/clash-linux-amd64-$CLASH_TUN_VERSION.gz"
+# CLASH_TUN_VERSION=$(curl -sL https://github.com/vernesong/OpenClash/raw/core/master/core_version | head -n 2 | tail -n 1)
+# CLASH_TUN_URL="https://github.com/vernesong/OpenClash/raw/core/master/premium/clash-linux-amd64-$CLASH_TUN_VERSION.gz"
 # openclash 的 Meta内核版本
 # CLASH_META_URL="https://github.com/vernesong/OpenClash/raw/core/master/meta/clash-linux-amd64.tar.gz"
 
@@ -39,8 +39,8 @@ CLASH_META_URL=$(curl -sL https://api.github.com/repos/MetaCubeX/Clash.Meta/rele
 
 
 # 给内核解压
-wget -qO- $CLASH_DEV_URL | tar xOvz > files/etc/openclash/core/clash
-wget -qO- $CLASH_TUN_URL | gunzip -c > files/etc/openclash/core/clash_tun
+# wget -qO- $CLASH_DEV_URL | tar xOvz > files/etc/openclash/core/clash
+# wget -qO- $CLASH_TUN_URL | gunzip -c > files/etc/openclash/core/clash_tun
 # wget -qO- $CLASH_META_URL | tar xOvz > files/etc/openclash/core/clash_meta
 
 # wget -qO- $CLASH_DEV_URL | gunzip -c > files/etc/openclash/core/clash
@@ -51,24 +51,16 @@ wget -qO- $CLASH_META_URL | gunzip -c > files/etc/openclash/core/clash_meta
 chmod +x files/etc/openclash/core/clash*
 
 
-# meta 要GeoIP.dat 和 GeoSite.dat
-# GEOIP_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat"
-# GEOSITE_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat"
-
+# 下载mihomo core需要的文件？
+# GeoIP.dat
 GEOIP_URL=https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/geoip.dat
+# GeoSite.dat
 GEOSITE_URL=https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/geosite.dat
+# Country.mmdb
+COUNTRY_FULL_URL=https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/country.mmdb
 
 wget -qO- $GEOIP_URL > files/etc/openclash/GeoIP.dat
 wget -qO- $GEOSITE_URL > files/etc/openclash/GeoSite.dat
-
-
-# Country.mmdb
-# COUNTRY_LITE_URL=https://raw.githubusercontent.com/alecthw/mmdb_china_ip_list/release/lite/Country.mmdb
-# COUNTRY_FULL_URL=https://raw.githubusercontent.com/alecthw/mmdb_china_ip_list/release/Country.mmdb
-
-COUNTRY_FULL_URL=https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/country.mmdb
-
-# wget -qO- $COUNTRY_LITE_URL > files/etc/openclash/Country.mmdb
 wget -qO- $COUNTRY_FULL_URL > files/etc/openclash/Country.mmdb
 
 echo "preset-clash-core executed successfully!"
