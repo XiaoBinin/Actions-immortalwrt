bbe5e065b543d4edd5a5c688e1c040ac759b9c4e
XiaoBinin <128011031+XiaoBinin@users.noreply.github.com>
2025-05-23 20:32:21 +0800

    尝试给files下的文件更改权限
    顺便修改执行顺序

--- scripts/init-settings.sh
+++ scripts/init-settings.sh
@@ -3,6 +3,8 @@
 #更改默认地址为192.168.8.1
 sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
 
+# 给config下的文件增加权限
+chmod 644 files/etc/config/*
 
 
 echo "init-settings executed successfully!"
