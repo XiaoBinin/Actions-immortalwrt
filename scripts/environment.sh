ee7af7ec2fb457cc787b72b9bd374286b41a18c7
XiaoBinin <128011031+XiaoBinin@users.noreply.github.com>
2025-12-25 11:01:56 +0800

    scripts : 编译工具恢复,golang回退使用feeds源码

--- scripts/environment.sh
+++ scripts/environment.sh
@@ -5,5 +5,5 @@ bzip2 ccache cmake cpio curl device-tree-compiler fastjar flex gawk gettext gcc-
 git gperf haveged help2man intltool libc6-dev-i386 libelf-dev libfuse-dev libglib2.0-dev libgmp3-dev \
 libltdl-dev libmpc-dev libmpfr-dev libncurses5-dev libncursesw5-dev libpython3-dev libreadline-dev \
 libssl-dev libtool lrzsz mkisofs msmtp ninja-build p7zip p7zip-full patch pkgconf python3 \
-python3-pyelftools python3-setuptools python3-socks python3-unidecode qemu-utils rsync scons \
+python3-pyelftools python3-setuptools qemu-utils rsync scons \
 squashfs-tools subversion swig texinfo uglifyjs upx-ucl unzip vim wget xmlto xxd zlib1g-dev
