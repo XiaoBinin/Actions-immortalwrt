515fd20d35b6993234ca936c69e346b8681f93df
XiaoBinin <128011031+XiaoBinin@users.noreply.github.com>
2026-01-07 16:53:55 +0800

    workflows : allwinner切换至openwrt-25.12源码

--- scripts/free_disk_space.sh
+++ scripts/free_disk_space.sh
@@ -29,21 +29,21 @@ echo "==========================================================================
 echo "Freeing up disk space on CI system"
 echo "=============================================================================="
 
-echo "Listing 100 largest packages"
+echo "清单100个最大的包"
 dpkg-query -Wf '${Installed-Size}\t${Package}\n' | sort -n | tail -n 100
 df -h
-echo "Removing large packages"
+echo "移除大的包裹"
 sudo apt-get remove -y '^dotnet-.*'
 sudo apt-get remove -y '^llvm-.*'
 sudo apt-get remove -y 'php.*'
 sudo apt-get remove -y '^mongodb-.*'
 sudo apt-get remove -y '^mysql-.*'
 sudo apt-get remove -y azure-cli google-cloud-sdk hhvm google-chrome-stable firefox powershell mono-devel
+# 自动清理依赖+缓存
 sudo apt-get autoremove -y
 sudo apt-get clean
 df -h
-echo "Removing large directories"
-
+echo "删除大目录"
 sudo rm -rf /usr/share/dotnet/
 sudo rm -rf /usr/local/graalvm/
 sudo rm -rf /usr/local/.ghcup/
@@ -51,4 +51,11 @@ sudo rm -rf /usr/local/share/powershell
 sudo rm -rf /usr/local/share/chromium
 sudo rm -rf /usr/local/lib/android
 sudo rm -rf /usr/local/lib/node_modules
-df -h
+sudo rm -rf /usr/share/dotnet
+sudo rm -rf /usr/local/lib/android
+sudo rm -rf /opt/ghc
+sudo rm -rf /usr/local/share/boost
+# 清理 Docker
+docker system prune -af --volumes
+# 删除 GitHub Actions 工具缓存
+sudo rm -rf /opt/hostedtoolcache
