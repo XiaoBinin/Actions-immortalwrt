329f32c85065bd753c5163da828417367c34a71f
XiaoBinin <128011031+XiaoBinin@users.noreply.github.com>
2026-01-07 08:40:28 +0800

    scripts : golang使用第三方源码

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -2,13 +2,13 @@
 
 #删除feeds中的插件
 # rm -rf ./feeds/packages/net/v2ray-geodata
-# rm -rf ./feeds/packages/lang/golang
+rm -rf feeds/packages/lang/golang
 rm -rf ./feeds/packages/net/{geoview,shadowsocks-libev,chinadns-ng,mosdns}
 rm -rf ./feeds/luci/applications/luci-app-mosdns
 
 #克隆依赖插件
 git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/pwpage
-# git clone https://github.com/sbwml/packages_lang_golang -b 25.x feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 26.x feeds/packages/lang/golang
 
 
 #克隆的源码放在small文件夹
