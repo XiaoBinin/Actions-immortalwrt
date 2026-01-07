8f19e3589a161ea81fa9e438a3765aee792a69d8
XiaoBinin <128011031+XiaoBinin@users.noreply.github.com>
2026-01-07 21:34:57 +0800

    Update packages.sh

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 #删除feeds中的插件
-# rm -rf ./feeds/packages/net/v2ray-geodata
 rm -rf feeds/packages/lang/golang
-rm -rf ./feeds/packages/net/{geoview,shadowsocks-libev,chinadns-ng,mosdns}
-rm -rf ./feeds/luci/applications/luci-app-mosdns
+rm -rf ./feeds/packages/net/{geoview,chinadns-ng,hysteria,mosdns,v2ray-geodata}
+rm -rf ./feeds/packages/net/{shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev}
+rm -rf ./feeds/packages/net/{sing-box,v2ray-geodata,v2ray-plugin,xray-core}
+rm -rf ./feeds/luci/applications/{luci-app-mosdns,luci-app-passwall}
 
 #克隆依赖插件
 git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/pwpage
@@ -21,22 +22,23 @@ git clone -b main --depth 1 https://github.com/sirpdboy/luci-app-timecontrol.git
 git clone -b 2024.09.05 --depth 1 https://github.com/XiaoBinin/luci-app-adguardhome.git
 #lucky
 git clone -b main --depth 1 https://github.com/gdy666/luci-app-lucky.git
-# #smartdns
-# git clone -b lede --depth 1 https://github.com/pymumu/luci-app-smartdns.git
+# smartdns
+# git clone -b master --depth 1 https://github.com/pymumu/luci-app-smartdns.git
 # git clone -b master --depth 1 https://github.com/pymumu/smartdns.git
-# #ssrp
+# ssrp
 # git clone -b master --depth 1 https://github.com/fw876/helloworld.git
-# #passwall
-# git clone -b main --depth 1 https://github.com/xiaorouji/openwrt-passwall.git
-#passwall2
+# passwall
+git clone -b main --depth 1 https://github.com/xiaorouji/openwrt-passwall.git
+# passwall2
 git clone -b main --depth 1 https://github.com/xiaorouji/openwrt-passwall2.git
-# #mosdns
+# mosdns
 git clone -b v5 --depth 1 https://github.com/sbwml/luci-app-mosdns.git
-# #openclash
+# openclash
 # git clone -b master --depth 1 https://github.com/vernesong/OpenClash.git
 # OpenWrt-nikki
 git clone --depth 1 https://github.com/nikkinikki-org/OpenWrt-nikki.git
-
+# daed
+git clone --depth 1 https://github.com/QiuSimons/luci-app-daed.git
 popd
 
 echo "packages executed successfully!"
