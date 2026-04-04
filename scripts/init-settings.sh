915f9a093c0a43eef966db707c52ff9f696065ea
XiaoBinin <128011031+XiaoBinin@users.noreply.github.com>
2026-04-04 09:28:33 +0800

    scripts : 默认ip尝试更改实现方式
    顺便补上immortalwrt.lan关联IP，参考来源https://github.com/VIKINGYFY/OpenWRT-CI/blob/main/Scripts/Settings.sh#L10

--- scripts/init-settings.sh
+++ scripts/init-settings.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 #更改默认地址为192.168.8.1
-sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
+DEF_IP="192.168.8.1"
+#修改immortalwrt.lan关联IP
+sed -i "s/192\.168\.[0-9]*\.[0-9]*/$DEF_IP/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
+sed -i "s/192\.168\.[0-9]*\.[0-9]*/$DEF_IP/g" package/base-files/files/bin/config_generate
+
 
 # 给config下的文件增加权限
 chmod 644 files/etc/config/*
